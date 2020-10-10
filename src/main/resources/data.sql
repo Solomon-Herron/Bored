11be446e611ed66ce8395ab5818230e06b83122d
Solomon Herron <solomonherron@gmail.com>
2020-10-10 15:06:28 -0500

    finito

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO activities(activity, description) VALUES('Take a jog', 'Run around your neighborhood for a natural high');
-INSERT INTO activities(activity, description) VALUES('Play basketball', 'Its fun');
-INSERT INTO activities(activity, description) VALUES('Hike', 'Even better than jogging');
-COMMIT;
